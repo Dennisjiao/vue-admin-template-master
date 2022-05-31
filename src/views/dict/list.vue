a18cac4a603ef875326bc9ac3f1a776a8074979f
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-05-31 17:55:39 +0100

    导入操作按钮和弹出框

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -7,12 +7,12 @@
                 <a href="http://localhost:8081/admin/cmn/dict/exportData" target="_blank"> <!--开个新的标签页，用户体验好一些-->
                     <el-button type="text"><i class="fa fa-plus"/> 导出</el-button>
                 </a>
-            <!-- <el-button type="text" @click="exportData"><i class="fa fa-plus"/> 导出</el-button> -->
+                <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
             </div>
         </div>
 
+        <!-- 导出操作 -->
         <el-table
-        
             :data="list" style="width: 100%" row-key="id" border lazy
             :load="getChildrens"
             :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
@@ -38,6 +38,27 @@
                 </template>
             </el-table-column>
         </el-table>
+
+        <!--如果multiple(dialogImportVisible)的值是false就不打开如果是true就打开弹窗。所以要在data中赋初值-->
+        <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+            <el-form label-position="right" label-width="170px">
+                <el-form-item label="文件">
+                    <el-upload
+                    :multiple="false"
+                    :on-success="onUploadSuccess"
+                    :action="'http://localhost:8081/admin/cmn/dict/importData'"
+                    class="upload-demo">
+                        <el-button size="small" type="primary">点击上传</el-button>
+                        <div slot="tip" class="el-upload__tip">只能上传excel文件, 且不超过500kb</div>
+                    </el-upload>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogImportVisible = false">
+                    取消
+                </el-button>
+            </div>
+        </el-dialog>
     </div>
 
 </template>
@@ -49,6 +70,7 @@ export default {
     data(){
         
         return{
+            dialogImportVisible:false,//设置弹窗是否弹出
             list:[] //定义数据字典列表数组
 
         }
@@ -56,10 +78,21 @@ export default {
 
     created(){
         this.getDictList(1)
-
     },
 
     methods: {
+        //导入数据字典  可以直接用ElementUI的上传组件 
+        importData(){
+            this.dialogImportVisible = true
+        },
+
+        onUploadSuccess(){
+            //关闭弹框
+            this.dialogImportVisible = false
+            //刷新
+            this.getDictList(1)
+        },
+
         //导出数据字典
         //直接调用接口就可以了，因为不用返回值
         exportData(){
