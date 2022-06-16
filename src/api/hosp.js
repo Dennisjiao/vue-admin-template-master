7dd068f73425b00fe57dc44201dd94ffd2b4e069
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-06-16 21:55:00 +0100

    add list of hospital detail

---
+++ src/api/hosp.js
@@ -0,0 +1,35 @@
+import request from '@/utils/request'
+
+export default {
+    //医院列表
+    getHospList(page,limit,searchObj){ //当前页，显示数量， 条件对象
+        return request({
+            //url: '/admin/hosp/hospitalSet/findPageHospSet/{current}/{limit}',
+            url: `/admin/hosp/hospital/list/${page}/${limit}`,
+
+            method: 'get',//get提交方式
+            params: searchObj
+            //data: searchObj,  //使用json进行传递, searchObj是接口对象
+            //params  //不用json进行传递
+        })
+    },
+
+    //根据dictcode查询所有节点
+    findByDictCode(dictCode){
+        return request({
+            url: `/admin/cmn/dict/findByDictCode/${dictCode}`,
+            method: 'get',//get提交方式
+        })
+    },
+
+    //根据数据id查询子数据列表
+    findChildId(id){
+        return request({
+            url: `/admin/cmn/dict/findChildData/${id}`,
+            method: 'get',//get提交方式
+        })
+    }
+
+
+
+}
