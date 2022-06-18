5f51afa4baa7338a75ddf2fff826c3a3f4eed417
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-06-18 14:08:47 +0100

    排班详情接口

--- src/api/hosp.js
+++ src/api/hosp.js
@@ -52,5 +52,15 @@ export default {
             url: `/admin/hosp/department/getDeptList/${hoscode}`,
             method: 'get'
     })
+    },
+
+    //查询预约规则
+    getScheduleRule(page, limit, hoscode, depcode){
+        return request({
+            url:`/admin/hosp/schedule/getScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
+            method:'get'
+        })
     }
+
+
 }
