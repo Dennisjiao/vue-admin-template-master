9e7284a536560938482d5c9268fc48568b3311ea
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-06-19 17:52:14 +0100

    更改端口对应springgateway端口号

--- src/views/hosp/schedule.vue
+++ src/views/hosp/schedule.vue
@@ -33,6 +33,36 @@
                 </el-row>
                 <el-row style="margin-top: 20px;">
                 <!-- 排班日期对应的排班医生 -->
+                <el-table
+                        v-loading="listLoading"
+                        :data="scheduleList"
+                        border
+                        fit
+                        highlight-current-row>
+                        <el-table-column
+                                    label="序号"
+                                    width="60"
+                                    align="center">
+                            <template slot-scope="scope">
+                                            {{ scope.$index + 1 }}
+                            </template>
+                        </el-table-column>
+                        <el-table-column label="职称" width="150">
+                            <template slot-scope="scope">
+                                            {{ scope.row.title }} | {{ scope.row.docname }}
+                            </template>
+                        </el-table-column>
+                        <el-table-column label="号源时间" width="80">
+                            <template slot-scope="scope">
+                                            {{ scope.row.workTime == 0 ? "上午" : "下午" }}
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="reservedNumber" label="可预约数" width="80"/>
+                        <el-table-column prop="availableNumber" label="剩余预约数" width="100"/>
+                        <el-table-column prop="amount" label="挂号费(元)" width="90"/>
+                        <el-table-column prop="skill" label="擅长技能"/>
+                </el-table>
+
                 </el-row>
             </el-main>
         </el-container>
@@ -61,7 +91,9 @@ export default {
 
             page: 1, // 当前页
             limit: 7, // 每页个数
-            total: 0 // 总页码
+            total: 0, // 总页码
+
+            scheduleList:[] //排班详情
 
         }
     },
@@ -71,6 +103,16 @@ export default {
         this.fetchData()
     },
     methods:{
+        //查询排班详情
+        getDetailSchedule(){
+            hospApi.getScheduleDetail(this.hoscode,this.depcode,this.workDate)
+                .then(response => {
+                    this.scheduleList = response.data
+                })
+        },
+
+
+
         fetchData() {
             hospApi.getDeptByHoscode(this.hoscode)
                 .then(response => {
@@ -82,6 +124,8 @@ export default {
 
                         this.getPage()
                     }
+                    //调用查询排班详情
+                    this.getDetailSchedule()
 
                 })
         },
@@ -122,8 +166,11 @@ export default {
     selectDate(workDate, index) {
         this.workDate = workDate
         this.activeIndex = index
+        
+        //调用查询排班详情
+        this.getDetailSchedule()
     },
-
+ 
     getCurDate() {
         var datetime = new Date()
         var year = datetime.getFullYear()
