4eabe429eb2da98613072b53e0325b94c7cff262
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-06-21 23:12:49 +0100

    Update dict.js

--- src/api/dict.js
+++ src/api/dict.js
@@ -5,7 +5,7 @@ export default {
     dictList(id){
         return request({
             //url: '/admin/hosp/hospitalSet/findPageHospSet/{current}/{limit}',
-            url: `/admin/cmn/dict/findChildData/${id}`,
+            url: `/admin/cmn/dict/findChildData/${id}`,//
 
             method: 'get',//post提交方式
             //data: searchObj,  //使用json进行传递, searchObj是接口对象
